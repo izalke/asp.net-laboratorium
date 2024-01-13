43a2f39c2bbbf0652e4ce834418defeca12dd692
Iza Skowrońska <31279258+izalke@users.noreply.github.com>
2024-01-13 17:12:48 +0100

    feat: migration database

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Data
+{
+	public class AppDbContext
+	{
+		public AppDbContext()
+		{
+		}
+	}
+}
+
---
+++ Data/Entities/ContactEntity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Data.Entities
+{
+    [Table("contacts")]
+    public class ContactEntity
+    {
+        public int Id { get; set; }
+        [MaxLength(50)]
+        [Required]
+        public string Name { get; set; }
+        [MaxLength(50)]
+        [Required]
+        public string Email { get; set; }
+        [MaxLength(12)]
+        [MinLength(9)]
+        public string Phone { get; set; }
+        [Column("birth_date")]
+        public DateTime Birth { get; set; }
+    }
+}
+
